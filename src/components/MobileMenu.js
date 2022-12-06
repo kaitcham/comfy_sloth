561ad112465be877b3cabc2ca8c773f4f978a046
Munezero Christian <chamsf5@aol.com>
2022-12-06 16:24:59 +0200

    Add navbar, footer and mobile menu components

---
+++ src/components/MobileMenu.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MobileMenu = () => <div>MobileMenu</div>;
+
+export default MobileMenu;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Navbar from './Navbar';
+import MobileMenu from './MobileMenu';
+import Footer from './Footer';
+
+export { Navbar, MobileMenu, Footer };
