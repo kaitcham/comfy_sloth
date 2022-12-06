7775b87e819afe6b9bca992cf056a542b83737fa
Munezero Christian <chamsf5@aol.com>
2022-12-06 16:28:15 +0200

    Add application routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar, Footer, MobileMenu } from './components';
+import {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Cart,
+  Checkout,
+  ErrorPage,
+} from './pages';
 
 const App = () => (
   <div>
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <Navbar />
+      <MobileMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<SingleProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </div>
 );
 
