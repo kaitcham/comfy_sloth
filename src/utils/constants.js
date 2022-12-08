909aa1123dc78daad642ca38e9fab3b72e19d656
Munezero Christian <chamsf5@aol.com>
2022-12-08 23:55:11 +0200

    Add links to constants

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'about',
+    url: '/about',
+  },
+  {
+    id: 3,
+    text: 'products',
+    url: '/products',
+  },
+];
