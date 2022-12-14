c488ed8dcc82224ab5bb036c5661ddc71d3937b4
Munezero Christian <chamsf5@aol.com>
2022-12-14 23:15:39 +0200

    Modify about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,5 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
+import PageHero from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
-const AboutPage = () => <div>AboutPage</div>;
+const AboutPage = () => <main>Page</main>;
 
+const Wrapper = styled.section`
+  display: grid;
+  gap: 4rem;
+  img {
+    width: 100%;
+    display: block;
+    border-radius: var(--radius);
+    height: 500px;
+    object-fit: cover;
+  }
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin: 0 auto;
+    margin-top: 2rem;
+    color: var(--clr-grey-5);
+  }
+  .title {
+    text-align: left;
+  }
+  .underline {
+    margin-left: 0;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+  }
+`;
 export default AboutPage;
