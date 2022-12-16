1a536f7c62dfd8b6cbec1d13dd06babd01a9ef72
Munezero Christian <chamsf5@aol.com>
2022-12-16 20:53:35 +0200

    Modify about page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import styled from 'styled-components';
-import PageHero from '../components';
+import PageHero from '../components/PageHero';
 import aboutImg from '../assets/hero-bcg.jpeg';
 
-const AboutPage = () => <main>Page</main>;
+const AboutPage = () => (
+  <main>
+    <PageHero title="about" />
+    <Wrapper className="page section section-center">
+      <img src={aboutImg} alt="nice desk" />
+      <article>
+        <div className="title">
+          <h2>our story</h2>
+          <div className="underline" />
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis eaque
+          et, enim unde veritatis tenetur atque explicabo pariatur culpa
+          deserunt quasi, placeat dignissimos cupiditate dicta aspernatur
+          aperiam blanditiis minima, quas dolores voluptatum tempore eum.
+          Temporibus ex blanditiis deleniti veritatis placeat ut, odit,
+          excepturi facere ipsa architecto in asperiores molestias tenetur ad
+          nemo veniam dicta reprehenderit aperiam iure? Doloremque, veniam!
+          Voluptatem odit cupiditate exercitationem laborum cumque consequatur
+          rerum, doloribus illum accusamus vel modi blanditiis veritatis?
+          Deleniti dolorum exercitationem quisquam blanditiis incidunt.
+        </p>
+      </article>
+    </Wrapper>
+  </main>
+);
 
 const Wrapper = styled.section`
   display: grid;
