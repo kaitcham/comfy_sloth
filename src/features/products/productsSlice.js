697cf0092c877090de9c65b01e82ecbe5ff4adcc
Munezero Christian <chamsf5@aol.com>
2022-12-20 23:14:30 +0200

    Add products in application global state

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,8 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { productsUrl as url } from '../../utils/constants';
 
 const initialState = {
   isMenuOpen: false,
+  loading: false,
+  products: [],
+  featuredProducts: [],
+  error: '',
 };
+
+export const fetchProducts = createAsyncThunk(
+  'products/fetchProducts',
+  async () => {
+    const response = await axios.get(url);
+    return response.data;
+  },
+);
 
 export const productsSlice = createSlice({
   name: 'products',
@@ -11,6 +25,24 @@ export const productsSlice = createSlice({
     toggleMenu: (state) => {
       state.isMenuOpen = !state.isMenuOpen;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.loading = false;
+        state.products = action.payload;
+        state.featuredProducts = action.payload.filter(
+          (product) => product.featured === true,
+        );
+        state.error = '';
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message;
+      });
   },
 });
 
