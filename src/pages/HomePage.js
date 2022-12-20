e1f03758e596b4d94b6475512897518782662781
Munezero Christian <chamsf5@aol.com>
2022-12-20 23:16:47 +0200

    Final landing page view

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import Services from '../components/Services';
+import Contact from '../components/Contact';
+import FeaturedProducts from '../components/FeaturedProducts';
 
-const HomePage = () => <div>HomePage</div>;
+const HomePage = () => (
+  <main>
+    <Hero />
+    <FeaturedProducts />
+    <Services />
+    <Contact />
+  </main>
+);
 
 export default HomePage;
