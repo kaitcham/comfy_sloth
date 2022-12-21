7c70bd3df17bbe8ac45f979db593d7343f11c718
Munezero Christian <chamsf5@aol.com>
2022-12-21 23:15:51 +0200

    Add single product url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -40,3 +40,5 @@ export const services = [
 ];
 
 export const productsUrl = 'https://course-api.com/react-store-products';
+
+export const singleProductUrl = 'https://course-api.com/react-store-single-product?id=';
