f9e6958d080231682706a30b1efcd730a319a7dc
Munezero Christian <chamsf5@aol.com>
2022-12-21 23:27:23 +0200

    Modify single product page to display all product images

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,5 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { Link, useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchSingleProduct } from '../features/singleProduct/singleProductSlice';
+import Loading from '../components/Loading';
+import Error from '../components/Error';
+import PageHero from '../components/PageHero';
+import ProductImages from '../components/ProductImages';
 
-const SingleProductPage = () => <div>SingleProductPage</div>;
+const SingleProductPage = () => {
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { loading, error, product } = useSelector(
+    (state) => state.singleProduct,
+  );
+
+  useEffect(() => {
+    dispatch(fetchSingleProduct(id));
+  }, [id]);
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <Error />;
+  }
+
+  const { name, images } = product;
+
+  return (
+    <Wrapper>
+      <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          Back to products
+        </Link>
+        <div className="product-center">
+          <ProductImages images={images} />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.main`
+  .product-center {
+    display: grid;
+    gap: 4rem;
+    margin-top: 2rem;
+  }
+  .price {
+    color: var(--clr-primary-5);
+  }
+  .desc {
+    line-height: 2;
+    max-width: 45em;
+  }
+  .info {
+    text-transform: capitalize;
+    width: 300px;
+    display: grid;
+    grid-template-columns: 125px 1fr;
+    span {
+      font-weight: 700;
+    }
+  }
+
+  @media (min-width: 992px) {
+    .product-center {
+      grid-template-columns: 1fr 1fr;
+      align-items: center;
+    }
+    .price {
+      font-size: 1.25rem;
+    }
+  }
+`;
 
 export default SingleProductPage;
