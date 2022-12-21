29099b1be7984cd453c7c615bc21057ec3ef7e86
Munezero Christian <chamsf5@aol.com>
2022-12-21 23:18:43 +0200

    Modify store to add single product slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../features/products/productsSlice';
+import singleProductReducer from '../features/singleProduct/singleProductSlice';
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
+    singleProduct: singleProductReducer,
   },
 });
 
