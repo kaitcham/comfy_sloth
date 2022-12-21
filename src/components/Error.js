deace73071ec73a68d8b635420fecb3ab7ab4328
Munezero Christian <chamsf5@aol.com>
2022-12-21 23:21:04 +0200

    Modify Error to use available space

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Error = () => (
-  <div className="section section-center text-center">
+  <div className="page-100 section section-center text-center">
     <h3>There was an error</h3>
   </div>
 );
