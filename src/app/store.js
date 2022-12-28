8875327516116b0d2d4f0b257918bc12a3604ebd
Munezero Christian <chamsf5@aol.com>
2022-12-28 22:59:50 +0200

    Add filtered products to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productsReducer from '../features/products/productsSlice';
 import singleProductReducer from '../features/singleProduct/singleProductSlice';
+import filteredProductsReducer from '../features/filteredProducts/filteredProductsSlice';
 
 const store = configureStore({
   reducer: {
     products: productsReducer,
     singleProduct: singleProductReducer,
+    filteredProducts: filteredProductsReducer,
   },
 });
 
