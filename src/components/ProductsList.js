15a343cf00f79861faa2891cae621f08abe45ec0
Munezero Christian <chamsf5@aol.com>
2022-12-29 23:23:11 +0200

    Display all products in the product list component

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,25 +3,15 @@ import propTypes from 'prop-types';
 import GridView from './GridView';
 import ListView from './ListView';
 
-const ProductList = ({ gridView, products }) => {
-  if (products.length < 1) {
-    return (
-      <h5 style={{ textTransform: 'none' }}>
-        Sorry, no products matched your search...
-      </h5>
-    );
-  }
-
-  return (
-    <div>
-      {gridView ? (
-        <GridView products={products} />
-      ) : (
-        <ListView products={products} />
-      )}
-    </div>
-  );
-};
+const ProductList = ({ gridView, products }) => (
+  <div>
+    {gridView ? (
+      <GridView products={products} />
+    ) : (
+      <ListView products={products} />
+    )}
+  </div>
+);
 
 ProductList.propTypes = {
   gridView: propTypes.bool.isRequired,
