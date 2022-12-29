d65d040e60a409d934005f0f70294dd1c3e607a8
Munezero Christian <chamsf5@aol.com>
2022-12-29 23:24:08 +0200

    Finalize product page

--- src/pages/ProductsPage.js
+++ src/pages/ProductsPage.js
@@ -1,5 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
+import Sort from '../components/Sort';
+import PageHero from '../components/PageHero';
+import Filters from '../components/Filters';
+import ProductsList from '../components/ProductsList';
+import { getProducts } from '../features/filteredProducts/filteredProductsSlice';
 
-const ProductsPage = () => <div>ProductsPage</div>;
+const ProductsPage = () => {
+  const dispatch = useDispatch();
+  const { gridView, filteredProducts: products } = useSelector(
+    (state) => state.filteredProducts,
+  );
+
+  React.useEffect(() => {
+    dispatch(getProducts());
+  }, []);
+
+  return (
+    <main>
+      <PageHero title="products" product={false} />
+      <Wrapper className="page">
+        <div className="section-center products">
+          <Filters />
+          <div>
+            <Sort />
+            <ProductsList gridView={gridView} products={products} />
+          </div>
+        </div>
+      </Wrapper>
+    </main>
+  );
+};
+
+const Wrapper = styled.div`
+  .products {
+    display: grid;
+    gap: 3rem 1.5rem;
+    margin: 4rem auto;
+  }
+  @media (min-width: 768px) {
+    .products {
+      grid-template-columns: 200px 1fr;
+    }
+  }
+`;
 
 export default ProductsPage;
