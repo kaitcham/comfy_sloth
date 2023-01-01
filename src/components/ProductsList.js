92171a4eb7c77a971e3b34ea5bb7f7c0420f4662
Munezero Christian <chamsf5@aol.com>
2023-01-01 11:26:19 +0200

    Add a loading spinner and conditional rendering

--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -3,19 +3,34 @@ import propTypes from 'prop-types';
 import GridView from './GridView';
 import ListView from './ListView';
 
-const ProductList = ({ gridView, products }) => (
-  <div>
-    {gridView ? (
-      <GridView products={products} />
-    ) : (
-      <ListView products={products} />
-    )}
-  </div>
-);
+const ProductList = ({ gridView, allProducts, filteredProducts }) => {
+  if (allProducts.length === 0 && filteredProducts.length === 0) {
+    return <div className="loading" />;
+  }
+
+  if (allProducts.length > 1 && filteredProducts.length === 0) {
+    return (
+      <h5 style={{ textTransform: 'none', textAlign: 'center' }}>
+        Sorry, no products matched your search...
+      </h5>
+    );
+  }
+
+  return (
+    <div>
+      {gridView ? (
+        <GridView filteredProducts={filteredProducts} />
+      ) : (
+        <ListView filteredProducts={filteredProducts} />
+      )}
+    </div>
+  );
+};
 
 ProductList.propTypes = {
   gridView: propTypes.bool.isRequired,
-  products: propTypes.arrayOf(propTypes.shape({})).isRequired,
+  allProducts: propTypes.arrayOf(propTypes.shape({})).isRequired,
+  filteredProducts: propTypes.arrayOf(propTypes.shape({})).isRequired,
 };
 
 export default ProductList;
