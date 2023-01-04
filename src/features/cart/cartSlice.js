ae1c037285da38cca43a92bf7414f14f1fda9523
Munezero Christian <chamsf5@aol.com>
2023-01-04 11:58:13 +0200

    Add functionalties to addToCart action

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,7 +10,36 @@ export const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
-    addToCart: (state, action) => {},
+    addToCart: (state, action) => {
+      const {
+        id, color, amount, product,
+      } = action.payload;
+      const itemToAdd = state.cart.find((item) => item.id === id + color);
+      if (itemToAdd) {
+        const newCart = state.cart.map((item) => {
+          if (item.id === id + color) {
+            let newAmount = item.amount + amount;
+            if (newAmount > item.max) {
+              newAmount = item.max;
+            }
+            return { ...item, amount: newAmount };
+          }
+          return item;
+        });
+        state.cart = newCart;
+      } else {
+        const newItem = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          max: product.stock,
+          price: product.price,
+          image: product.images[0].url,
+        };
+        state.cart = [...state.cart, newItem];
+      }
+    },
   },
 });
 
