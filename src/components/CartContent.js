bf88daf9b14d000933913d16f1e1b614d5c5275f
Munezero Christian <chamsf5@aol.com>
2023-01-04 13:12:26 +0200

    Create cart content component

---
+++ src/components/CartContent.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import CartColumns from './CartColumns';
+import CartItem from './CartItem';
+import CartTotals from './CartTotals';
+
+const CartContent = ({ cart }) => (
+  <Wrapper className="section section-center">
+    <CartColumns />
+    {cart.map((item) => (
+      <CartItem key={item.id} item={item} />
+    ))}
+    <hr />
+    <div className="link-container">
+      <Link to="/products" className="link-btn">
+        continue shopping
+      </Link>
+      <button type="button" className="link-btn clear-btn">
+        clear cart
+      </button>
+    </div>
+    <CartTotals />
+  </Wrapper>
+);
+const Wrapper = styled.section`
+  .link-container {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2rem;
+  }
+  .link-btn {
+    background: transparent;
+    border-color: transparent;
+    text-transform: capitalize;
+    padding: 0.25rem 0.5rem;
+    background: var(--clr-primary-5);
+    color: var(--clr-white);
+    border-radius: var(--radius);
+    letter-spacing: var(--spacing);
+    font-weight: 400;
+    cursor: pointer;
+  }
+  .clear-btn {
+    background: var(--clr-black);
+  }
+`;
+
+CartContent.propTypes = {
+  cart: propTypes.arrayOf(propTypes.shape({})).isRequired,
+};
+
+export default CartContent;
