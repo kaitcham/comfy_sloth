8d3b94f455430d9f36e14652f5af76d54b847b8e
Munezero Christian <chamsf5@aol.com>
2023-01-04 11:19:23 +0200

    Add cart reducer to the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from '../features/cart/cartSlice';
 import productsReducer from '../features/products/productsSlice';
 import singleProductReducer from '../features/singleProduct/singleProductSlice';
 import filteredProductsReducer from '../features/filteredProducts/filteredProductsSlice';
 
 const store = configureStore({
   reducer: {
+    cart: cartReducer,
     products: productsReducer,
     singleProduct: singleProductReducer,
     filteredProducts: filteredProductsReducer,
